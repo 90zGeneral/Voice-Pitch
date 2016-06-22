c98a03ac26973422480c22718c849b6e7744bb4e
Roydon Jeffrey <brushdam14823@gmail.com>
2016-06-22 15:26:14 -0400

    Configured segue btw viewControllers, add play back sound buttons and link to rightful IBOutlets and IBActions, and preparing to record and save audio file with complete url file path

---
+++ Voice Pitch/PlaySoundsViewController.swift
@@ -0,0 +1,65 @@
+//
+//  PlaySoundsViewController.swift
+//  Voice Pitch
+//
+//  Created by Roydon Jeffrey on 6/22/16.
+//  Copyright © 2016 Italyte. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var darthVaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioURL: NSURL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: NSTimer!
+    
+    enum ButtonType: Int {
+        case Slow = 0, Fast, ChipMunk, Vader, Echo, Reverb
+    }
+    
+    var recordedAudio: NSURL!
+    
+    @IBAction func playSoundForButton(sender: UIButton) {
+        print("play sound button pressed")
+    }
+    
+    @IBAction func stopButtonPressed(sender: UIButton) {
+        print("stop audio button pressed")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Voice Pitch/RecordSoundsViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  Voice Pitch
+//
+//  Created by Roydon Jeffrey on 6/21/16.
+//  Copyright © 2016 Italyte. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    @IBOutlet var recordLabel: UILabel!
+    @IBOutlet var recordButton: UIButton!
+    @IBOutlet var stopRecordingButton: UIButton!
+    
+    var audioRecorder: AVAudioRecorder!
+    
+    @IBAction func record(sender: AnyObject) {
+        
+        recordButton.enabled = false
+        stopRecordingButton.enabled = true
+        recordLabel.text = "Recording in progress"
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
+    }
+    
+    @IBAction func stopRecording(sender: AnyObject) {
+        
+        recordButton.enabled = true
+        stopRecordingButton.enabled = false
+        recordLabel.text = "Tap to Record"
+        
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopRecordingButton.enabled = false
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        print("AVAudioRecorder finished saving the recording")
+        
+        if flag {
+            self.performSegueWithIdentifier("stopRecording", sender: audioRecorder.url)
+        }else {
+            print("Failure to successfully save recording")
+        }
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destinationViewController as! PlaySoundsViewController
+            
+            let recordedAudio = sender as! NSURL
+            
+            playSoundsVC.recordedAudio = recordedAudio
+        }
+    }
+
+
+}
+
